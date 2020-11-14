0a55c702db1207099c6647ef2a43d08d6e3ce03c
Sushma-9803 <apsushma.xworkz@gmail.com>
2020-11-14 11:58:54 +0530

    adding DataType files

---
+++ Num.java
@@ -0,0 +1,16 @@
+class Numbers
+{
+ public static void main(String a[])
+{
+  int age=23;
+  int AGE=24;
+  int agE=25;
+  int AGe=26;
+  int AgE=27;
+  System.out.println(age);
+System.out.println(AGE);
+System.out.println(agE);
+System.out.println(AGe);
+System.out.println(AgE);
+}
+}
