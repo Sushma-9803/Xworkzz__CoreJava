13eebf55f85916f6807cf5bbe996893d5190b832
Sushma-9803 <apsushma.xworkz@gmail.com>
2020-11-14 12:03:39 +0530

    adding DataType files

---
+++ redbull.java
@@ -0,0 +1,13 @@
+class Person
+{
+  public static void main(String a[])
+{
+   float weight=40.5;
+   float calories=14.5;
+   float height=6.1
+   System.out.printl(weight);
+   System.out.println(calories);
+   System.out.println(height);
+    
+}
+}
