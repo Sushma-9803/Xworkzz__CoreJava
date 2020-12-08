fa507e5e475f9982190e90a8b47c4fc95755ce5e
Sushma-9803 <apsushma.xworkz@gmail.com>
2020-12-08 12:33:35 +0530

    adding files

---
+++ arrays-project/src/com/xworkz/Mentos.java
@@ -0,0 +1,15 @@
+package com.xworkz;
+
+public class Mentos {
+	public static void main(String[] args) {
+		int mentos[]= {21,22,23,24,25};
+		System.out.println(mentos[0]);
+		System.out.println(mentos[1]);
+		System.out.println(mentos[2]);
+		System.out.println(mentos[3]);
+		System.out.println(mentos[4]);
+		System.out.println(mentos.length);
+		
+	}
+
+}
