56aeec85677294e24f4d13b5123f93a0e974d47b
Sushma-9803 <apsushma.xworkz@gmail.com>
2020-12-09 22:45:49 +0530

    adding files

---
+++ institute-proj/src/com/xworkz/RevaUniversityutil/RevaUniversityUtil.java
@@ -0,0 +1,12 @@
+package com.xworkz.RevaUniversityutil;
+
+import com.xworkz.institute.RevaUviversity;
+
+public class RevaUniversityUtil {
+	public static void main(String[] args) {
+		
+		RevaUviversity reva=new RevaUviversity("yelahanka","BE");
+		System.out.println(reva.address+" "+reva.course);
+	}
+
+}
---
+++ institute-proj/src/com/xworkz/institute/Institute.java
@@ -0,0 +1,15 @@
+package com.xworkz.institute;
+
+public class Institute {
+	public String address;
+	public String course;
+	public Institute(String address,String course)
+	{
+		this.address=address;
+		this.course=course;
+	}
+	public Institute()
+	{
+
+}
+}
