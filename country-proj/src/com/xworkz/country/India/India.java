ff4b97d5f60c30566a039ee6a5d9b01b2bf22594
Sushma-9803 <apsushma.xworkz@gmail.com>
2020-12-09 22:43:09 +0530

    adding files

---
+++ country-proj/src/com/xworkz/country/India/India.java
@@ -0,0 +1,13 @@
+package com.xworkz.country.India;
+
+import com.xworkz.country.Country;
+
+public class India extends Country {
+
+	public India(int noOfStates, int noofLanguages, String capital, int noOfLanguages2) {
+		super(noOfStates, noofLanguages, capital, noOfLanguages2);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
+	
