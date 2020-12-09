0c3df3c4c3d7da4f68e0c4c8312711ff18c497ee
Sushma-9803 <apsushma.xworkz@gmail.com>
2020-12-09 23:53:48 +0530

    Adding files

---
+++ wrapper-project/src/com/xworkz/Wrapper.java
@@ -0,0 +1,20 @@
+package com.xworkz;
+
+public class Wrapper {
+	public static void main(String[] args) {
+		Integer myInt=456;
+		Character myChar='d';
+		Long myLong=567854987L;
+		Short myShort=428;
+		Double myDouble=45.00;
+		String myString="sush";
+		Float myFloat=6.5f;
+		System.out.println(myInt);
+		System.out.println(myChar);
+		System.out.println(myLong);
+		System.out.println(myShort);
+		System.out.println(myDouble);
+		System.out.println(myString);
+		System.out.println(myFloat);
+	}
+}
