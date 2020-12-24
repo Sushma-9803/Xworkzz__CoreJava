fb6ec37e57632cb401780d468649d1a268d5721c
Sushma-9803 <apsushma.xworkz@gmail.com>
2020-12-24 17:59:25 +0530

    adding files

---
+++ watch-proj/src/com/xworkz/xworkzapp/watch/Rolex.java
@@ -0,0 +1,24 @@
+package com.xworkz.xworkzapp.watch;
+
+import java.util.Date;
+
+public class Rolex extends Watch {
+	public Rolex()
+	{
+		System.out.println("rolex abject is created");
+	}
+	 public void displayTime()
+	 {
+		
+		 System.out.println("23/12/2020");
+	 }
+	/* public void time()
+	 {
+		 Date date=new Date();
+		 System.out.println(date);
+	 }
+	 */
+	
+	 
+
+}
