0a75f2d3841f2bcdb53398fc6e97d7ae5399e77f
Sushma-9803 <apsushma.xworkz@gmail.com>
2020-12-24 20:59:48 +0530

    adding files

---
+++ fruit-proj/src/com/xworkz/xworkzapp/fruit/Dragon.java
@@ -0,0 +1,16 @@
+package com.xworkz.xworkzapp.fruit;
+
+
+
+public class Dragon extends Fruit {
+	public Dragon()
+	{
+		System.out.println("dragonfruit abject is created");
+	}
+	 public void makeHappy()
+	 {
+		
+		 System.out.println("happy");
+	 }
+	
+}
