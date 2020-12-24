81beebf40d0507339819f7eead36ee550f5fb62c
Sushma-9803 <apsushma.xworkz@gmail.com>
2020-12-24 18:03:14 +0530

    adding files

---
+++ fruit-proj/src/com/xworkz/xworkzapp/fruit/Fruit.java
@@ -0,0 +1,26 @@
+package com.xworkz.xworkzapp.fruit;
+
+
+
+public class Fruit {
+
+	public String color;
+	public double price;
+	
+	public Fruit()
+	{
+		System.out.println("fruit object ic created");
+	}
+	
+	
+	 public void  makeHappy()
+	   {
+	   
+		    System.out.println("tasty");
+	   }
+
+	
+
+}
+
+
